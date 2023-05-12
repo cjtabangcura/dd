854a17812f1a75554478af9010bb7de705bc4ca0
cjtabangcura <cjtabangcura28@gmail.com>
2023-05-13 01:59:57 +0800

    Update DBConnection.php

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -4,10 +4,10 @@ if(!defined('DB_SERVER')){
 }
 class DBConnection{
 
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
+    private $host = localhost;
+    private $username = u605986563_case2;
+    private $password = Legalcase123;
+    private $database = u605986563_case2;
     
     public $conn;
     
