2217d67b4863ffe470321c772127b27d28bc4bee
cjtabangcura <cjtabangcura28@gmail.com>
2023-05-13 08:56:50 +0800

    f

--- includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$self = $_SERVER['PHP_SELF'];
-if(!strpos($self, "login.php") && !strpos($self, "registration.php")){
-    if(!isset($_SESSION['userdata']['id'])){
-        redirect('./login.php');
-    }
-}
-?>
